889cdfab79f9b07f45653b591fdc3f2ab789154b
nikolasrieble <nikolas-rieble@gmx.de>
2020-01-01 16:42:33 +0100

    adds formatting features

---
+++ google-quest-challenge/Features.py
@@ -0,0 +1,33 @@
+import string
+
+
+def character_density(search_for, search_in):
+    return search_in.count(search_for) / len(search_in)
+
+
+def punctuation_density(text: str):
+    return sum([character_density(symbol, text) for symbol in string.punctuation])
+
+
+class FormatFeatures:
+    def __init__(self):
+        # https://stackoverflow.com/editing-help
+        # Maybe use regex here to identy specific pattern instead of only usage of these symbols
+        self.formatting_pattern = [
+            # Code
+            "    ",
+            "'''",
+            "~~~",
+            "`",
+            # String formatting
+            "*",
+            "**",
+            "***"
+            # TODO: add further formatting pattern here
+        ]
+
+    def patternExists(self, text):
+        return [pattern in text for pattern in self.formatting_pattern]
+
+    def patternDensity(self, text):
+        return [text.count(pattern) for pattern in self.formatting_pattern]/ len(text)
