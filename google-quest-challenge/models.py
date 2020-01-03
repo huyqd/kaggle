fd96d94443785ee70e28a27988d6319d1d468b63
Nikolas Rieble <nikolas.rieble@scalable.capital>
2020-01-03 17:17:34 +0100

    Random Model

--- google-quest-challenge/models.py
+++ google-quest-challenge/models.py
@@ -4,25 +4,21 @@ import numpy as np
 import scipy as scp
 
 
-def randomize(test: pd.DataFrame):
-    """ Take a dataframe and randomize the result """
-    sample_submission = pd.read_csv(root_path / 'data' / 'google-quest-challenge' / 'sample_submission.csv')
-    required_output_cols = sample_submission.columns
+class RandomModel:
+    def __init__(self):
+        self.model = None
 
-    n_output_rows, n_output_cols = test.shape[0], 30
+    def train(self, X, y):
+        self.model = 1
 
-    # Retrieve columns to predict
-    predict_cols = required_output_cols[-n_output_cols:]
+    def predict(self, X):
+        assert self.model is not None
+        return np.random.random(len(X), 30)
 
-    # Get the result
-    randomized_res = np.random.rand(n_output_rows, n_output_cols)
-    randomized_res = pd.DataFrame(randomized_res, columns=predict_cols)
-    randomized_res = pd.concat([test, randomized_res], axis=1, sort=False)
-    randomized_res = randomized_res.reindex(columns=required_output_cols)
+    def save(self, path):
+        # TODO Implement
+        pass
 
-    return randomized_res
-
-
-if __name__ == '__main__':
-    test = pd.read_csv(root_path / 'data' / 'google-quest-challenge' / 'test.csv')
-    res = randomize(test)
+    def load(self, path):
+        # TODO Implemnett
+        pass
